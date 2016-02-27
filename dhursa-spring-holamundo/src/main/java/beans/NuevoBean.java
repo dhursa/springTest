420e45820807263b176893d3db338b458d1a39a9
dhursa <dhursa@yahoo.com>
2016-02-27 11:49:47 -0500

    Nuevo

---
+++ dhursa-spring-holamundo/src/main/java/beans/NuevoBean.java
@@ -0,0 +1,15 @@
+package beans;
+
+public class NuevoBean {
+	
+	private String NuevoMensaje;
+
+	public String getMensaje1() {
+		return NuevoMensaje;
+	}
+
+	public void setMensaje1(String NuevoMensaje) {
+		this.NuevoMensaje = NuevoMensaje;
+	}
+
+}
